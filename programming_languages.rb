4e052cdf336e72f64777e35d3a4050feb8ee6b61
Eric Razor <eric.razorjr@gmail.com>
2018-05-14 01:52:08 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,14 @@
+require 'pry'
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  key_counter=0
+  val_counter = 0
+  languages.each do |oo_or_functional, language_hash|
+    language_hash.each do |language, lang_attr_hash|
+      lang_attr_hash.each do |lang_type, lang_str|
+
+      end
+    end
+  end
+  return new_hash
 end
